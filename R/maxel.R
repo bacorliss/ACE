c73730bb263bbc0aeabd0ce10853482289055dd2
Bruce Corliss <bac7wj@virginia.edu>
2019-08-27 13:55:46 -0400

    Conitnued basic plotting, figuring out what organization of figures would be.

--- R/maxel.R
+++ R/maxel.R
@@ -1,24 +1,54 @@
 
 # Load packages
-library(readr)
-library(dplyr)
+#library(readr)
+#library(dplyr)
 library(ggplot2)
 library(tibble)
 
+nsamples = 15
+ngroups = 2
+set.seed(0)
 
 # Sample two null distributions
-null_dists = tibble(x1 = rnorm(10, 0, 1), x2 = rnorm(10, 0, 1))
+null_dist = tibble(group = c(rep(0,nsamples), rep(1,nsamples)),
+                   y = rnorm(ngroups*nsamples, 0, 1))
 
-
-
-boxplot(null_dists,x1,x2)
 
 
 
 # Basic box plot
-p <- ggplot(null_dists, aes(x=x1, y=x2)) + 
-  geom_boxplot() + geom_jitter()
+p <- ggplot(null_dist, aes(x=group,y=y)) + 
+  geom_boxplot(aes(group=group,y=y)) + geom_jitter(width = .1)
+# Fill in boxplot
+p+scale_fill_brewer(palette="Dark2")
 p
 
-# Box plot with dot plot
-p + geom_dotplot(binaxis='y', stackdir='center', dotsize=1)
+
+
+
+# 1B: distribution of effect between distributions
+
+## 1A-C: One sample distribution case
+
+## 1D-F: two sample distribution case
+
+## 1G-I: 3 sample distirbution case
+
+
+
+
+
+
+
+
+
+
+
+# MAXEL correlate with effect size
+
+# MAXEL rewards experiments with lower noise
+
+# MAXEL rewards experiment with lower bias
+
+# MAXEL does not need a multiple test pvalue correction
+
