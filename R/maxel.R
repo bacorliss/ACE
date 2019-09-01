20079e12419d62667589fae3f7aa568d8a023efe
Bruce Corliss <bacorliss@gmail.com>
2019-09-01 18:20:29 -0400

    Initial pdf plotting figured out, now trying to setup and render a table of latec equations or R expression.

--- R/maxel.R
+++ R/maxel.R
@@ -13,36 +13,52 @@ nsamples = 15
 ngroups = 2
 set.seed(0)
 
+equ_tbl = data.frame(maxel = c(expression(beta +frac(miles, gallon)),
+                     expression(beta +frac(miles, gallon)),
+                     expression(beta +frac(miles, gallon))), 
+           rel_maxel = c(expression(beta +frac(miles, gallon)), 
+                         expression(beta +frac(miles, gallon))),
+           expression(beta +frac(miles, gallon)))
 
 
-# Plot 2 distributions
-p9 <- ggplot(data.frame(x = c(-4, 4)), aes(x = x)) +
-  stat_function(fun = dt, args = list(df = 8))
-p9
+data.frame()
+ggplot(as.data.frame(table(df)), aes(x=gender, y = Freq, fill=fraud)) + 
+  geom_bar(stat="identity")
 
 
-pdf_tbl = rbind(
-  tibble(group="X1", x=seq(-5,5,.01) ,
-         y=dnorm(seq(-5,5,.01), mean = 0, sd = 1, log = FALSE)),
-  tibble(group="X2", x=seq(-5,5,.01) ,
-         y=dnorm(seq(-5,5,.01), mean = 2, sd = 1, log = FALSE)))
-p <- ggplot(pdf_tbl, aes(x=x,y=y,fill=group)) +
-  geom_area(aes(fill=aes(group))) +
-  geom_smooth(method="auto") 
-p
 
+# Case 3: Unpaired data, two distributions
+# 3.1 distribution A, B
+x <- seq(-3,6,.01)
+mu <- c(1,2)
+data3_1 = rbind(
+  tibble(group=factor("1"), x=x,
+         y=smooth.spline(x,dnorm(x, mean = mu[1], sd = 1, log = FALSE))$y),
+  tibble(group=("2"), x=x,
+         y=smooth.spline(x,dnorm(x, mean = mu[2], sd = 1, log = FALSE))$y))
 
-p9 <- ggplot(data.frame(x = c(0, 1)), aes(x = x)) +
-  stat_function(fun = dnorm, args = list(0.2, 0.1),
-                colour = color_pal[1]) +
-  stat_function(fun = dnorm, args = list(0.7, 0.05),
-                colour = color_pal[2]) +
-  scale_x_continuous(name = "Probability",
-                     breaks = seq(0, 1, 0.2),
-                     limits=c(0, 1)) +
-  scale_y_continuous(name = "Frequency") +
-  ggtitle("Normal function curves of probabilities")
-p9
+p3_1 <- ggplot(data3_1, aes(x=x, y=y, group=group, fill=group)) +
+  geom_line(size=1,aes(color=group)) + 
+  geom_ribbon(data3_1=subset(data3_1,x>-4 & x<7.5), 
+              aes(x=x,ymax=y),ymin=0,alpha=0.3) +
+  annotate("text", x=mu[1], y=0.4+.02, label= "mu[1]",
+           parse=TRUE,color=color_pal[1],size=8) +
+  annotate("text", x=mu[2], y=0.4+.02, label= "mu[2]",
+           parse=TRUE,color=color_pal[2],size=8)
+
+# 3.2: difference between distributions
+x3_2 <- seq(-3,6,.01)
+data3_2 = tibble(group=factor("1"), x=x3_2,
+         y=smooth.spline(x,dnorm(x, mean = mu[2]-mu[1], sd = 1, log = FALSE))$y)
+tt_3_2 = tidy(t.test(rnorm(1000, mean=mu[2]-mu[1], sd=1)))
+
+p3_2 <-ggplot(data3_2, aes(x=x, y=y, group=group, fill=group, color=color_pal[3])) +
+  geom_line(size=1,color=color_pal[3]) + 
+  geom_ribbon(ymax = data3_2$y,ymin=0, alpha = 0.3, fill=color_pal[3],color=color_pal[3]) +
+  annotate("text", x=mu[2]-mu[1], y=0.4+.02, label= "mu[d] == mu[a] - mu[b]",
+           parse=TRUE,color=color_pal[3],size=8) +
+  geom_vline(xintercept=tt_3_2$conf.low) +
+  geom_vline(xintercept=tt_3_2$conf.high)
 
 
 
