b76f92bd1dd8dfe9d2d9418561730d6147bb05f6
Bruce Corliss <bac7wj@virginia.edu>
2019-10-02 22:00:26 -0400

    Continued development of MHD.

--- R/mhd.R
+++ R/mhd.R
@@ -20,12 +20,14 @@ t_dist_2s_unpaired <- function(x, y, alpha = 0.05) {
   return(ucl)
 }
 
+
 t_dist_2s_paired <- function(x, y, alpha = 0.05) {
   # Pairwise difference between observations
   d = x-y
   ucl = t_dist_1s(d, alpha)
   return(ucl)
 }
+
 
 t_dist_1s <- function(x, alpha = 0.05) {
 # Calculates upper bounds of mean of folded t-distribution based on pre-folded 
@@ -43,7 +45,37 @@ t_dist_1s <- function(x, alpha = 0.05) {
 }
 
 
+z_dist_1s <- function(x, alpha = 0.05) {
+  # Calculates most hidden difference from a 1-sample z-distribution
+  # Inputs
+  #  x: sample of observations
+  # Outputs
+  #  ucl: upper confidence limit of mean of folded absolute value z distribution
+  # Sample statistics
+  x_bar <- mean(x)
+  sx <- sd(x)
+  nx <- length(x)
+  dfx <- n - 1
+  
+  # Calculate upper confidence limit of mean of folded distribition
+  ucl = folded_z_dist(x_bar, sx, nx, dfx, alpha)
+  
+  return(ucl)
+}
+
+
 folded_t_dist <- function(x_bar, sx, nx, dfx, alpha = 0.05) {
+  #  Caclulate t statistic from integrating a central normal pdf shifted by -x_bar
+  # Using minimization function to integrate using a normal CDF setting the area 
+  # under the curve to (1-a)
+  # Inputs
+  #   x_bar: sample mean
+  #   sx: Sample standard deviation
+  #   nx: sample number
+  #   dfx: sample degrees of freedom
+  # Output
+  #   ucl: upper confidence limit of the mean of folded distribution
+  
   # Estimate multiplier for bounds of search interval
   sd_mult <- qt(1 - alpha, dfx)
   
@@ -67,26 +99,22 @@ folded_t_dist <- function(x_bar, sx, nx, dfx, alpha = 0.05) {
 }
 
 
-z_dist_1s <- function(x, alpha = 0.05) {
-  # Calculates upper bounds of mean of folded t-distribution based on pre-folded 
-  # central t-distribution
-  
-  # Sample statistics
-  x_bar <- mean(x)
-  s <- sd(x)
-  n <- length(x)
-  df <- n - 1
-  
-  # Calculate upper confidence limit of mean of folded distribition
-  ucl = folded_z_dist(x_bar, sx, nx, dfx, alpha)
-    
-    return(ucl)
-}
  
 
 folded_z_dist <- function(x_bar, sx, nx, dfx, alpha = 0.05) { 
+  #  Caclulate z statistic from integrating a central t-distribution pdf shifted by -x_bar
+  # Using minimization function to integrate using a t-distribution CDF setting the area 
+  # under the curve to (1-a)
+  # Inputs
+  #   x_bar: sample mean
+  #   sx: Sample standard deviation
+  #   nx: sample number
+  #   dfx: sample degrees of freedom
+  # Output
+  #   ucl: upper confidence limit of the mean of folded distribution
+  
   # Estimate multiplier for bounds of search interval
-  sd_mult <- qt(1 - alpha, dfx)
+  sd_mult <- qnorm(1 - alpha, dfx)
   
   # Integration of folded t-distribution can be calculate from standard central t-distribution
   z_star_function <- function(x) {abs(pnorm(-x_bar + x,sx) - pnorm(-x_bar - x, sx) - (1 - alpha))}
--- R/mhd_test.R
+++ R/mhd_test.R
@@ -6,7 +6,7 @@ library(broom)
 
 x <- rnorm(10, mean = 0, sd = 2)
 
-# Mean Shift to zero
+# Mean Shift samples to zero
 x <- x - mean(x)
 
 
@@ -17,8 +17,12 @@ print(sprintf("MHD UCL: %.3f,  Ttest CI upper: %.3f", ucl, t_mean_ucl$conf.high)
 
 
 # Test base case of centrally aligned sample, where normal CI and UCL_
-ucl <- t_dist_1s(x+1)
-t_mean_uci = tidy(t.test(x+1, conf.level = 0.95))
-t_mean_uci = tidy(t.test(x+1,, conf.level = 0.975))
+ucl <- t_dist_1s(x)
+t_mean_uci_95 = tidy(t.test(x, conf.level = 0.95))
+t_mean_uci_975 = tidy(t.test(x,, conf.level = 0.975))
 
 print(sprintf("MHD UCL: %.3f,  Ttest CI upper: %.3f", ucl, t_mean_uci$conf.high))
+
+
+
+
