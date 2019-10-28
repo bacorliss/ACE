7e2f992014fe7456d5e02ecbec18f1898ac29af4
bacorliss <bruce.a.corliss@gmail.com>
2019-10-28 13:53:02 -0400

    Continued plotting of MHD vs CI comparison.

--- R/mhd_test.R
+++ R/mhd_test.R
@@ -10,6 +10,7 @@ library(grid)
 library(gridExtra)
 library(colorspace)
 library("RColorBrewer")
+library(cowplot)
 clr_set = brewer.pal(n = 8, name = "Set1")
 # Calculate conifdence intervals
 source("R/mhd.R")
@@ -66,9 +67,8 @@ df$ttest_p_val  <- apply(y_sweep, 1, function (x)  t.test(x)$p.value )
 x_critical <-  RootSpline1(x=df$x,y=df$ttest_p_val,y0 = 0.05)
 
 ## Subplot A,B: MHD[a] transitions from CI[a] to CI[a/2] as the sample mean departs from zero 
-# plot
-p1_1 = ggplot(data=df,mapping = aes(x=x,y=mhd_95))
-p1_1 <- p1_1 + theme_classic() +
+g1A = ggplot(data=df,mapping = aes(x=x,y=mhd_95))
+g1A <- g1A + theme_classic() +
   geom_rect(aes(xmin=-Inf, xmax=x_critical[1], ymin=-Inf, ymax=Inf), fill = lighten('black',0.9)) +
   geom_rect(aes(xmin=x_critical[2], xmax=Inf, ymin=-Inf, ymax=Inf), fill = lighten('black',0.9)) +
   geom_line(aes(x=x,y=mcl_95, linetype = "CI_97.5"), col=lighten("blue",0.6), size=.8) +
@@ -84,33 +84,43 @@ p1_1 <- p1_1 + theme_classic() +
     axis.title.x=element_blank(), axis.text.x=element_blank(),axis.ticks.x=element_blank(),
     legend.position = "right",legend.justification = c(0, 0.5), legend.title = element_blank(),
     legend.margin = margin(c(0, 0, 0, 0)), plot.margin = unit(c(0,0,0,0),"mm"), legend.box = "vertical", 
-    legend.text=element_text(size=8))
-p1_1
+    legend.text=element_text(size=8)) + 
+  scale_y_continuous(labels=function(x) sprintf("%.1f", x))
+g1A
 
 # Plot p value of sample sweep
-p1_2 = ggplot(data=df,mapping = aes(x=x,y=ttest_p_val))
-p1_2 <- p1_2 +
+g1B = ggplot(data=df,mapping = aes(x=x,y=ttest_p_val))
+g1B <- g1B +
   geom_rect(aes(xmin=-Inf, xmax=x_critical[1], ymin=-Inf, ymax=Inf,fill = "Crit. Region")) +
   geom_rect(aes(xmin=x_critical[2], xmax=Inf, ymin=-Inf, ymax=Inf,fill = "Crit. Region")) +
-  geom_point(aes(col="t-test "), shape=1) +
+  geom_hline(aes(yintercept=0.05, col="Conf. Level")) + 
+  geom_point(aes(col="t-test "), shape = 16, size = 1) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black")) + 
-  geom_hline(aes(yintercept=0.05, col="Conf. Level")) + 
-  xlab(expression(bar(x))) + ylab("p value") + 
+  xlab(expression(bar(x))) +
+  scale_y_continuous(name="p value", limits=c(0, 1.1),breaks=seq(0, 1, .5)) +
   scale_shape_manual("", values = c(2,2)) +
   scale_linetype_manual("",values=c("dotted", "dotted"))+
-  scale_color_manual("", labels = c("Conf. Level","t-test "),values=c('red','black'))+
+  scale_color_manual("", labels = c("Conf. Level   ","t-test "),values=c('red','black'))+
   scale_fill_manual("",values = lighten('black',0.9), guide = guide_legend(override.aes = list(alpha = 1))) + 
   theme(
     legend.position = "right", legend.justification = c(0, 0.5), legend.title = element_blank(),
     legend.margin = margin(c(0, 0, 0, 0)), plot.margin = unit(c(0,0,0,0),"mm"), legend.box = "vertical",
     legend.text=element_text(size=8))
-p1_2
+g1B
+
+
+
+cg = plot_grid(g1A, g1B, label_size = 12, ncol=1,rel_heights = c(.5,.5) )
+
+save_plot("figure/figure_2AB_MHD_vs_CI95.pdf", cg, ncol = 1, nrow = 1, base_height = 1.5,
+          base_asp = 3, base_width = 6, dpi = 300) # paper="letter"
+
 
 # Assemble grid for plotting
 gs <- lapply(1:2, function(ii) grobTree(rectGrob(gp = gpar(alpha = 0.5))))
-gs[[1]] = p1_1 #+ labs(tag = expression(bold(A)))
-gs[[2]] = p1_2 #+ labs(tag = expression(bold(B)))
+gs[[1]] = g1A #+ labs(tag = expression(bold(A)))
+gs[[2]] = g1B #+ labs(tag = expression(bold(B)))
   
 # Arrange grob obejcts into grid
 gt <- arrangeGrob(grobs = gs, layout_matrix = rbind(c(1),
@@ -121,10 +131,15 @@ gt$heights <- unit(c(.6, .4), "npc")
 # Apply row and column heights to gtable
 gt_pad <- gtable::gtable_add_padding(gt, unit(0, "inch"))
 
+maxWidth = grid::unit.pmax(g1B$widths[2:5], g1B$widths[2:5])
+gg1A$widths[2:5] <- as.list(maxWidth)
+gg1B$widths[2:5] <- as.list(maxWidth)
+grid.arrange(g1A, g1B, ncol=1)
+
 # Export figure to disk
 ggsave("figure/figure_2AB_MHD_vs_CI95.pdf", gt_pad, device = "pdf", path = NULL,
        scale = 1, width = 7, height = 2.5, units = "in",
-       dpi = 300, limitsize = TRUE,paper="letter")
+       dpi = 300, limitsize = TRUE,)
   
 
 
@@ -133,7 +148,7 @@ ggsave("figure/figure_2AB_MHD_vs_CI95.pdf", gt_pad, device = "pdf", path = NULL,
 #--------------------------------------------------------------------------------------#
 # Generate 1000 samples, loop through different shifts, and quantify MHD, UCL_95, UCL_90
 source("R/mhd.R")
-mu = c(-10,-1, -.5, 0, .5, 1, 10)
+mu = c(NA, -10,-1, -.5, 0, .5, 1, 10)
 sigma = 1
 n_samples = 100
 n_obs = 5
@@ -151,9 +166,15 @@ conf_range_fcn = function(x, alpha)  c(qt(1-(alpha/2), df = length(x)-1) * sd(x)
 df_list <- list()
 
 for (n in seq(1,length(mu),1)) {
-  shift = mu[n]
-  y_sweep = y_samples+mu[n]
   
+  shift = mu[n]
+  
+  if (is.na(shift)) {
+    y_sweep = y_samples - rowMeans( y_samples)
+    shift <- "Centered"
+  }else {
+    y_sweep = y_samples+shift
+  }
   
   # Calcualte most hidden difference
   mhd_95    <- apply(y_sweep, 1, mhd_1sample)
@@ -178,10 +199,28 @@ for (n in seq(1,length(mu),1)) {
 df <- ldply(df_list, rbind)
 
 
-h1 <- ggplot(df, aes(x=mu, y=normalized_mhd_95)) + 
-  geom_boxplot(aes(group=n))+ facet_grid(. ~ n, scales = "free_y")
 
-h1
+
+# revalue(df, c(="Centered"))
+
+g1C <- ggplot(df, aes(x=mu, y=normalized_mhd_95)) + 
+  geom_hline(yintercept=1, col=lighten("blue",0.6), linetype = "solid", size=0.8) + 
+  geom_hline(yintercept=0, col=lighten("blue",0.6), linetype = "dashed", size=0.8) +
+  geom_boxplot(aes(group=n)) + 
+  facet_grid(. ~ n, scales = "free",switch = "y")
+
+gg1C <- ggplotGrob(g1C)
+
+gg1C$widths[6] = 6*gg1C$widths[6]
+grid.draw(gg1C)
+
+# Export figure to disk
+ggsave("figure/figure_2C_MHD_vs_CI95.tif", gg1C, device = "tif", path = NULL,
+       scale = 1, width = 5, height = 1.5, units = "in",
+       dpi = 300, limitsize = TRUE,)
+
+
+
 
 sample_data %>%
   group_by(Location) %>%                       
@@ -192,11 +231,8 @@ library(tidyr)
 library(broom)
 dt_res <- df %>%
   group_by(mu) %>%                      
-  summarise_each(funs(mean, sd, pvalue = t.test(normalized_mhd_95,mu=0)$p.value),normalized_mhd_95) %>%
-  unnest()
+  summarise_each(funs(mean, sd, pvalue = t.test(normalized_mhd_95,mu=0)$p.value),normalized_mhd_95) 
 
 
 
-
-bp
 
