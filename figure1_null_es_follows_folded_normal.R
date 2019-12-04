de1cad4f29312b74a699d2a2e08c9b462279f555
Bruce Corliss <bac7wj@virginia.edu>
2019-12-04 13:44:24 -0500

    Started fihgure 1.

---
+++ figure1_null_es_follows_folded_normal.R
@@ -0,0 +1,24 @@
+
+
+# The magntiude of effect sizefrom normal data follows the folded normal 
+# distribution and converges to the normal distribution as mu>>sigma
+
+
+
+
+
+# 1A Show examples of histograms of normals transformed into 
+
+
+
+# Test to show that generated samples follow folded normal
+
+
+
+# Quantile plot that shows samples follows folded normal
+
+
+# Show how mean is changed as x_bar increases from zero
+
+
+# Show how std increases as x_bar changes from zero
