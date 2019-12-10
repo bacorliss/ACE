cb879664290c0bb411fb281318e61c9c8dc62740
Bruce Corliss <bac7wj@virginia.edu>
2019-12-10 14:13:34 -0500

    Coded first subfigure for exploring MHD, and explored, code for KS test between Folded Normal and abs(Normal)

--- figure1_null_es_follows_folded_normal.R
+++ figure1_null_es_follows_folded_normal.R
@@ -6,19 +6,107 @@
 
 
 
+# Load packages
+library(ggplot2)
+library(tibble)
+library(RColorBrewer)
+library(broom)
+library(gridExtra)
+library(grid)
+library(rlang)
+library(colorspace)
+library(VGAM)
+library(boot)
+library(dplyr)
+library(cowplot)
+
+# choose colors for plotting
+color_pal = brewer.pal(4, "Set1")
+
+
+# Parameters
+set.seed(0)
+dist_text_size=3;
+title_font_size=9;
+equ_font_size=8;
+
+# Initial parameters
+nsamples = 1e6
+ngroups = 2
+set.seed(0)
+dist_text_size=3;
+title_font_size=9;
+equ_font_size=8;
+
+
 
 # 1A Show examples of histograms of normals transformed into 
+sample_1a = rnorm(nsamples, mean = 0, sd = 1)
+df_1a = rbind(tibble(group = 2, mu = "mu==0", x = sample_1a    ), tibble(group = 1, mu = "mu==0", x = abs(sample_1a)    ),
+              tibble(group = 2, mu = "mu==1", x = sample_1a + 1), tibble(group = 1, mu = "mu==1", x = abs(sample_1a + 1)),
+              tibble(group = 2, mu = "mu==1.5", x = sample_1a + 1.5), tibble(group = 1, mu = "mu==1.5", x = abs(sample_1a + 1.5)),
+              tibble(group = 2, mu = "mu==3", x = sample_1a + 3), tibble(group = 1, mu = "mu==3", x = abs(sample_1a + 3))
+              )
+df_1a$group <- as.factor(df_1a$group)
+df_1a$mu    <- as.factor(df_1a$mu)
+
+df_1a %>%
+  group_by(group,mu) %>%
+  summarise(mean = mean(x), n = n())
+
+p_1a <- ggplot(df_1a, aes(x = x)) +
+  geom_histogram(aes(fill = group,y=..density..), binwidth = .2, boundary = 0, position = "identity", alpha = 0.5) + 
+  facet_grid(. ~ mu, scales = "free_x", labeller=label_parsed) +
+  theme_classic() + ylab("Probability") +
+  scale_fill_discrete(name = "", labels = c(expression(f(x)), expression(abs(phantom(0)*f(x)~phantom())))) + 
+  theme(plot.title = element_text(hjust = 0.5, size = title_font_size,family="serif"),legend.position = "none",
+        text = element_text(size=9))
+# p_1a
+# Export to TIF
+save_plot("figure/figure_1a_example_dists.tiff", p_1a, ncol = 1, nrow = 1, base_height = 1.5,
+          base_asp = 3, base_width = 3.5, dpi = 600) # paper="letter"
+
 
 
 
 # Test to show that generated samples follow folded normal
+#-------------------------------------------------------------------------
+
+# Randomly generate normal data for     
+#     Normal data,                      |
+#     Normal data, then apply ABS()     |   x   1000
+#     Folded normal data                |
+mus <- runif(1e4, min = -5, max = 5)
+n_obs <- 100
+
+
+norm <- sapply(mus, function (x) rnorm(n_obs, mean=x, sd=1),simplify = TRUE)
+abs_norm <- sapply(mus, function (x) rnorm(n_obs, mean=x, sd=1),simplify = TRUE)
+fnorm <- sapply(mus, function (x) rfoldnorm(n_obs, mean=x, sd=1),simplify = TRUE)
+
+# Test if norm is different from abs_norm
+norm2fnorm <- mapply(function(x,y) ks.test(x, y, alternative = "two.sided", exact = TRUE, 
+                                           tol=1e-8, simulate.p.value=FALSE)$p.value, norm, fnorm,SIMPLIFY = TRUE)
+
+# Test if abs_norm is different from fnorm
+abs_norm2fnorm <- mapply(function(x,y) ks.test(x, y, alternative = "two.sided", exact = TRUE, 
+                                           tol=1e-8, simulate.p.value=FALSE)$p.value, abs_norm, fnorm,SIMPLIFY = TRUE)
+
+
+# Plot difference between P(N) and P(F[N]) over mu
+
+
+# Fit line to difference and show 95% CI is equal to zero
+
 
 
 
 # Quantile plot that shows samples follows folded normal
 
 
+
 # Show how mean is changed as x_bar increases from zero
+
 
 
 # Show how std increases as x_bar changes from zero
