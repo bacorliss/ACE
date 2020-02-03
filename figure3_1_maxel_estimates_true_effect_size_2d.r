25344e7be702f746d18a23de42c225184af86197
Bruce Corliss <bruce.a.corliss@gmail.com>
2020-02-03 16:46:05 -0500

    Cleaning up figure 3 code. May need a FDR correction for testing if error rate is equal to 0 or alpha.

--- figure3_1_maxel_estimates_true_effect_size_2d.r
+++ figure3_1_maxel_estimates_true_effect_size_2d.r
@@ -1,5 +1,6 @@
 
 
+# p.adjust(p, method = p.adjust.methods, n = length(p))
 
 
 # Load packages
@@ -24,22 +25,38 @@ library(equivalence)
 # https://cran.rstudio.com/web/packages/TOSTER/vignettes/IntroductionToTOSTER.html
 library(TOSTER)
 
-source("R/mhd.r")
 
 # Helper Functions
-RowVar <- function(x, ...) {
-  rowSums((x - rowMeans(x, ...))^2, ...)/(dim(x)[2] - 1)
+source("R/mhd.r")
+RowVar <- function(x, ...) rowSums((x - rowMeans(x, ...))^2, ...)/(dim(x)[2] - 1)
+# Test if a value is within an interval (inclusive)
+within_ci <- function(ci, x) x >= ci[1] & x <= ci[2]
+error_rate_test <-function(mhd, mu,n_samples) {
+# Test if proportion of MHDs that are above mu (error rate) are equal to 0.05 
+# and 0.00, return code do delineate combinations of both results
+#  (0) E != 0.05, E != 0.00,  (1) E == 0.05, E != 0.00
+#  (2) E != 0.05, E == 0.00,  (3) E == 0.05, E == 0.00
+  error_rate_ci <- binom.test(sum(mhd < abs(mu)), n_samples, p = 0.05, 
+                              alternative = "two.sided", conf.level = 0.95)[4][[1]]
+  is_error_rate_p05 <- within_ci(error_rate_ci, 0.05)
+  is_error_rate_0 <- within_ci(error_rate_ci,   0)
+  # Assign code to different situations with error test
+  error_rate_tests = 0
+  if (is_error_rate_p05 & is_error_rate_0) { error_rate_tests = 3 }
+  else if (is_error_rate_0) { error_rate_tests = 2 }
+  else if (is_error_rate_p05) { error_rate_tests = 1 }
+  return(error_rate_tests)
 }
-within_ci <- function(ci,x) x>=ci[1] & x <=ci[2]
 
-
-
+# General variables
 fig_basename = "f_3"
 n_samples <- 1e3
-rand_seed <-0
+rand_seed <- 0
 
 
-# 2D visualization of MHD differecne and error rate over mu and sigma
+
+
+# 2D visualization of MHD difference and error rate over mu and sigma
 #                                                                              #
 #______________________________________________________________________________#
 mus <- seq(-2.5, 2.5, by = .1)
@@ -52,19 +69,14 @@ df_2d <- tibble(mean_diff_mhd_mu = matrix(0L, nrow = length(sigmas), ncol = leng
                 mean_rdiff_mhd_mu = matrix(0L, nrow = length(sigmas), ncol = length(mus)),
                 mean_mhd_error = matrix(0L, nrow = length(sigmas), ncol = length(mus)),
                 error_rate_tests = matrix(0L, nrow = length(sigmas), ncol = length(mus)))
-
-
-
 # Store results to disk since calculations are significant
 set.seed(rand_seed)
 if (!file.exists("temp/MHD_Error_2D.rds")) {
-  
   # Generate random samples based on random mu values
   for (c in seq(1, length(mus), by = 1)) {
     for (r in seq(1, length(sigmas), by = 1)) {
       # Get samples, where each row is a seperate sample, columns are observations
-      x1 <-
-        matrix(rnorm(n_samples * n_obs, mus[c], sigmas[r]), nrow = n_obs)
+      x1 <- matrix(rnorm(n_samples * n_obs, mus[c], sigmas[r]), nrow = n_obs)
       
       # Calculate the MHD
       mhd = apply(x1, 2, function (x) mhd_1sample_normal(x, alpha = 0.05))
@@ -78,28 +90,17 @@ if (!file.exists("temp/MHD_Error_2D.rds")) {
       df_2d$mean_mhd_error[r, c] <- n_successes / n_samples
       
       # Test if binomial coeefficient of error rate is equal to 1 or alpha
-      error_rate_ci <-
-        binom.test(sum(mhd < abs(mus[c])), n_samples, p = 0.05, 
-                   alternative = "two.sided", conf.level = 0.95)[4][[1]]
-      is_error_rate_p05 <- within_ci(error_rate_ci, 0.05)
-      is_error_rate_0 <- within_ci(error_rate_ci,   0)
-      if (is_error_rate_p05 & is_error_rate_0) {
-        df_2d$error_rate_tests[r, c] = 3
-      } else if (is_error_rate_0) {
-        df_2d$error_rate_tests[r, c] = 2
-      } else if (is_error_rate_p05) {
-        df_2d$error_rate_tests[r, c] = 1
-      }
-
+      df_2d$error_rate_tests[r, c] = error_rate_test(mhd, mus[c],n_samples)
+      
+      
+      # Fraction of monte carlo tries where MHD was in error
       df_2d$mean_mhd_error[r, c] <- sum(mhd < abs(mus[c])) / n_samples
     }
   }
-  
   # Replace INF with NaNs
   df_2d$mean_rdiff_mhd_mu[!is.finite(df_2d$mean_rdiff_mhd_mu)] <- NaN
   # Save an object to a file
   saveRDS(df_2d, file = "temp/MHD_Error_2D.rds")
-  
   
 } else {
   # Restore the object
@@ -194,15 +195,12 @@ dev.off()
 png(paste("figure/", fig_basename, "a4 MHD error test.png"),    
     width = 2.3*300, height = 3*300, res = 300, pointsize = 8)       
 # creates a own color palette from red to green
-my_palette <- colorRampPalette(c("white","blue", "red", "purple"))(n = 299)
-# (optional) defines the color breaks manually for a "skewed" color transition
-# col_breaks = c(seq(0, .5, length=100), 
-#                seq(0.51, 1.5, length=100),
-#                seq(1.51,2,length=100))             
-
+my_palette <- colorRampPalette(c("white","blue", "red", "purple"))(n = 399)
+col_breaks = c(seq(0, .5, length=100), seq(0.6, 1.5, length=100), 
+               seq(1.6,2.5,length=100), seq(2.6,3.5,length=100))    
 heatmap.2(df_2d$error_rate_tests, Rowv=FALSE, Colv=FALSE, trace="none", dendrogram = "none", 
           labRow=rev(round(sigmas,1)), labCol= round(mus,1),
-          col = my_palette,
+          col = my_palette, breaks = col_breaks,
           density.info='none', scale="none",
           cexRow = 1, cexCol = 1,
           denscol="black", keysize=1,
@@ -222,9 +220,6 @@ dev.off()
 
 
 
-
-
-
 # 2D visualization of MHD difference and error rate over sigma and mu/sigma
 #                                                                              #
 #______________________________________________________________________________#
@@ -233,7 +228,6 @@ sigmas <- seq(.1, 5, by = .1)
 mu_ov_sigmas <- seq (-.5, .5, by=0.01)
 # n_samples <- 1e4
 n_obs <- 50
-
 # Matrix diff and error of MHD
 df_2d <- tibble(mean_diff_mhd_mu = matrix(0L, nrow = length(sigmas), 
                                           ncol = length(mu_ov_sigmas)),
@@ -243,7 +237,6 @@ df_2d <- tibble(mean_diff_mhd_mu = matrix(0L, nrow = length(sigmas),
                                         ncol = length(mu_ov_sigmas)),
                 error_rate_tests = matrix(0L, nrow = length(sigmas), 
                                           ncol = length(mu_ov_sigmas)))
-
 
 # Store results to disk since calculations are significant
 set.seed(rand_seed)
@@ -255,12 +248,8 @@ if (!file.exists("temp/MHD_Error_2D_mu_ov_sigmas.rds")) {
     mus = mu_ov_sigmas * sigmas[r]
     
      for (c in seq(1, length(mu_ov_sigmas), by = 1)) {
-    
-      
-      
       # Get samples, where each row is a seperate sample, columns are observations
-      x1 <-
-        matrix(rnorm(n_samples * n_obs, mus[c], sigmas[r]), nrow = n_obs)
+      x1 <- matrix(rnorm(n_samples * n_obs, mus[c], sigmas[r]), nrow = n_obs)
       
       # Calculate the MHD
       mhd = apply(x1, 2, function (x)
@@ -274,14 +263,14 @@ if (!file.exists("temp/MHD_Error_2D_mu_ov_sigmas.rds")) {
       n_successes <- sum(mhd < abs(mus[c]))
       df_2d$mean_mhd_error[r, c] <- n_successes / n_samples
       
+      # Test if binomial coeefficient of error rate is equal to 1 or alpha
+      df_2d$error_rate_tests[r, c] = error_rate_test(mhd, mus[c],n_samples)
     }
   }
-  
   # Replace INF with NaNs
   df_2d$mean_rdiff_mhd_mu[!is.finite(df_2d$mean_rdiff_mhd_mu)] <- NaN
   # Save an object to a file
   saveRDS(df_2d, file = "temp/MHD_Error_2D_mu_ov_sigmas.rds")
-  
   
 } else {
   # Restore the object
@@ -290,33 +279,44 @@ if (!file.exists("temp/MHD_Error_2D_mu_ov_sigmas.rds")) {
 
 
 
+# 2D Heatmap of error (1) p == 0.05), (2) p == 1, (3) p == 0.05 & p == 1
+png(paste("figure/", fig_basename, "a5 MHD error test mu_over_sigma.png"),    
+    width = 2.3*300, height = 3*300, res = 300, pointsize = 8)       
+# creates a own color palette from red to green
+my_palette <- colorRampPalette(c("white","blue", "red", "purple"))(n = 399)
+col_breaks = c(seq(0, .5, length=100), seq(0.6, 1.5, length=100), 
+               seq(1.6,2.5,length=100), seq(2.6,3.5,length=100))   
+heatmap.2(df_2d$error_rate_tests, Rowv=FALSE, Colv=FALSE, trace="none", dendrogram = "none", 
+          labRow=rev(round(sigmas,1)), labCol= round(mu_ov_sigmas,2),
+          col = my_palette, breaks = col_breaks,
+          density.info = 'none', scale = "none", cexRow = 1, cexCol = 1,
+          denscol = "black", keysize = 1, key = FALSE,
+          lmat = rbind(c(5, 4, 2), c(6, 1, 3)), margins=c(3,0),
+          lhei = c(2, 6), lwid = c(1, 10, 1),
+          na.color = "black", main = NULL,
+          xlab(expression(mu)), ylab(expression(sigma)))
+dev.off()
 
 
 
 
 # Visualize error rate of MHD
-png(paste("figure/", fig_basename, "a5 MHD error rate 2D.png"),    
+png(paste("figure/", fig_basename, "a5 MHD error rate 2D mu_over_sigma.png"),    
     width = 2.3*300, height = 3*300, res = 300, pointsize = 8)       
 # creates a own color palette from red to green
 my_palette <- colorRampPalette(c("blue","white", "red"))(n = 299)
 # (optional) defines the color breaks manually for a "skewed" color transition
 col_breaks = c(seq(0, 0.039, length=100), seq(0.04, 0.06, length=100),seq(0.061, 0.08,length=100)) 
-
 color_cull <- function(x) x[seq(1,round(length(x)*.73), by = 1)]
 heatmap.2(df_2d$mean_mhd_error, Rowv = FALSE, Colv = FALSE, trace = "none", dendrogram = "none", 
           labRow = rev(round(sigmas,1)), labCol = round(mu_ov_sigmas,2),
-          col = my_palette,
-          density.info='none', scale="none",
+          col = my_palette, density.info='none', scale="none",
           cexRow = 1, cexCol = 1, denscol="black", keysize=1, 
           key.par = list( mar = c(3.5,0,3,0)),
           lmat = rbind(c(5, 4, 2), c(6, 1, 3)), margins = c(3,0),
           lhei = c(2, 6), lwid = c(1, 10, 1),
-          breaks = col_breaks,
-          key.title = "",
-          key.xlab = "",
-          main = NULL, 
-          xlab( expression(mu)), 
-          ylab( expression(sigma)))
+          breaks = col_breaks, key.title = "", key.xlab = "",
+          main = NULL, xlab( expression(mu)), ylab( expression(sigma)))
 dev.off()
 
 
