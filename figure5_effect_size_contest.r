ccb89238f0961ecc1e5f656571b6640fcf6362f7
bacorliss <bruce.a.corliss@gmail.com>
2020-07-01 22:10:49 -0400

    Finished heatmap graphics for overall effect size contest restults.

--- figure5_effect_size_contest.r
+++ figure5_effect_size_contest.r
@@ -109,7 +109,7 @@ df_init <- generateExperiment_Data(n_samples, n_obs, n_sims, rand.seed,
                                    mus_1a  = rep(1,n_sims), 
                                    sigmas_1a = rep(1,n_sims),
                                    mus_1d  = rep(2,n_sims), 
-                                   sigmas_1d = runif(n_sims, 5, 50),
+                                   sigmas_1d = runif(n_sims, 5, 30),
                                    
                                    mus_2a  = rep(25,n_sims), 
                                    sigmas_2a = rep(1,n_sims),
@@ -394,93 +394,90 @@ dfs_6b_rsigma <- process_esize_simulations(df_init, gt_colname = gt_colnames[2],
 
 
 
+# Save and load effect size contest data
+save.image(file=paste("figure/", fig_basename, "_effect_size_contest_results.png"))
+load(paste("figure/", fig_basename, "_effect_size_contest_results.png"))
 
 
 
 
 
-# Export summary stats for scale data
-dfs_scale <- c(ls(pat="dfs_[1-3]a"),ls(pat="dfs_[1-3]b"))
-scale_norm_ind = rep(c(1,3,1,3),2)
-
-# Extract means for each group and subtract from 0.5 (random)
-scale_means_from_0.5 <- 0.5 - sapply(dfs_scale, function(x) get(x)$df_plotted$mean)
-scale_scores <- t(t(scale_means_from_0.5)/scale_means_from_0.5[cbind(scale_norm_ind,seq_along(scale_norm_ind))])
-# Export csv
-rownames(scale_scores) <- effect_size_dict[[4]]
-# Get statistical significance
-scale_scores_sig <- !sapply(dfs_scale, function(x) get(x)$df_plotted$is_mean_0.5) #& scale_scores >0
-rownames(scale_scores_sig) <- effect_size_dict[[4]]
-
-
-#https://stackoverflow.com/questions/45890845/highlight-subset-cells-from-heatmap-by-row-col-index
-
-
-# Function for making selection rectangles around selection cells
-# Function for making selection rectangles around selection cells
-makeRects <- function(cells){
-  coords = expand.grid(dim(cells)[1]:1, 1:dim(cells)[2])[cells,]
-  xl=coords[,2]-0.49; yb=coords[,1]-0.49
-  xr=coords[,2]+0.49; yt=coords[,1]+0.49
-  rect(xl,yb,xr,yt,border="black",lwd=1)
-}
-
-
+# OVerall contest heatmaps
+#------------------------------------------------------------------------
 
 # Re-make heatmap with rectangles based on the selection
 my_palette <- colorRampPalette(c(rgb(255, 0, 0,maxColorValue = 255),"white",
                                  rgb(47, 117, 181,maxColorValue = 255)))(n = 299)
 col_breaks = c(seq(-1, -.1, length=100), seq(-.09, 0.09, length=100), 
                seq(0.1, 1.0,length=100))
+# Function for making selection rectangles around selection cells
+makeRects <- function(cells,lwd){
+  coords = expand.grid(dim(cells)[1]:1, 1:dim(cells)[2])[cells,]
+  xl=coords[,2]-0.49; yb=coords[,1]-0.49; xr=coords[,2]+0.49; yt=coords[,1]+0.49
+  rect(xl,yb,xr,yt,border="black",lwd=lwd)
+}
+make2RectGroups <- function(cells1,lwd1, cells2, lwd2){
+  makeRects(cells1,lwd1)
+  makeRects(cells2,lwd2)
+}
 
 
-heatmap.2(scale_scores, trace = "none", dendrogram = "none", key = FALSE, Rowv=F, Colv=F,
-          add.expr = {makeRects(scale_scores_sig)}, col = my_palette, 
+
+
+# Export summary stats for scale data
+#-------------------------------------------------------------------------------
+dfs_scale <- c(ls(pat="dfs_[1-3]a"),ls(pat="dfs_[1-3]b"))
+scale_norm_ind = rep(c(1,3,1,3),2)
+
+# Extract means for each group and subtract from 0.5 (random)
+scale_means_from_0.5 <- 0.5 - sapply(dfs_scale, function(x) get(x)$df_plotted$mean)
+scale_scores <- t(t(scale_means_from_0.5)/
+                    scale_means_from_0.5[cbind(scale_norm_ind,seq_along(scale_norm_ind))])
+# Export csv
+rownames(scale_scores) <- effect_size_dict[[4]]
+# Get statistical significance
+scale_scores_sig <- !sapply(dfs_scale, function(x) get(x)$df_plotted$is_mean_0.5) 
+scale_score_norm <- sapply(scale_norm_ind, function(ind,len) 
+  ifelse(1:len == ind, TRUE,FALSE), length(effect_size_dict[[4]]))
+
+png(paste("figure/", fig_basename, "es_contest scale.png"),    
+    width = 5*300, height = 2.5*300, res = 300, pointsize = 8)  
+heatmap.2(scale_scores, trace = "none", dendrogram = "none", key = FALSE,
+          add.expr = {make2RectGroups(scale_scores_sig,1,scale_score_norm,3)}, 
+          col = my_palette,  Rowv=F, Colv=F, sepwidth=c(0,0),
           labRow =  sapply(effect_size_dict[[4]], function(x) parse(text=x)),labCol = "",
-          cellnote=matrix(sapply(scale_scores,function(x) sprintf("%0.2+f",x)),nrow = dim(scale_scores)[1]),
-          notecol="black",notecex=0.75,row_names_side="left")
+          cellnote=matrix(sapply(scale_scores,function(x) sprintf("%0.2+f",x)),
+                          nrow = dim(scale_scores)[1]),
+          notecol="black",notecex=1, lwid=c(0.01,5),lhei=c(0.01,5),margins =c(.1,.1))
+dev.off()
 
-
-write.table(scale_scores,scale_contest_path, sep=",")
-write.table(scale_scores_sig,scale_contest_path, sep=",", append=TRUE)
-
+# Export summary stats for relative scale data
+#-------------------------------------------------------------------------------
 # Export summary stats for relative scale data
 dfs_rscale <- c(ls(pat="dfs_[4-6]a"),ls(pat="dfs_[4-6]b"))
 rscale_norm_ind = rep(c(2,4,2,4),2)
 # Extract means for each group and subtract from 0.5 (random)
 rscale_means_from_0.5 <- 0.5 - sapply(dfs_rscale, function(x) get(x)$df_plotted$mean)
-rscale_scores <- t(t(rscale_means_from_0.5)/rscale_means_from_0.5[cbind(rscale_norm_ind,seq_along(rscale_norm_ind))])
+rscale_scores <- t(t(rscale_means_from_0.5)/
+                     rscale_means_from_0.5[cbind(rscale_norm_ind,seq_along(rscale_norm_ind))])
 # Export csv
 rownames(rscale_scores) <- effect_size_dict[[2]]
 # Get statistical significance
-rscale_scores_sig <- !sapply(dfs_rscale, function(x) get(x)$df_plotted$is_mean_0.5)
-rownames(rscale_scores_sig) <- effect_size_dict[[2]]
+rscale_scores_sig <- !sapply(dfs_rscale, function(x) get(x)$df_plotted$is_mean_0.5) 
+rscale_score_norm <- sapply(rscale_norm_ind, function(ind,len) 
+  ifelse(1:len == ind, TRUE,FALSE), length(effect_size_dict[[4]]))
 
-write.table(rscale_scores,rscale_contest_path, sep=",")
-write.table(rscale_scores_sig,rscale_contest_path, sep=",",append=TRUE)
+png(paste("figure/", fig_basename, "es_contest relative scale.png"),    
+    width = 5*300, height = 2.5*300, res = 300, pointsize = 8)  
+heatmap.2(rscale_scores, trace = "none", dendrogram = "none", key = FALSE,
+          add.expr = {make2RectGroups(rscale_scores_sig,1,rscale_score_norm,3)}, 
+          col = my_palette,  Rowv=F, Colv=F, sepwidth=c(0,0),
+          labRow =  sapply(effect_size_dict[[4]], function(x) parse(text=x)),labCol = "",
+          cellnote=matrix(sapply(rscale_scores,function(x) sprintf("%0.2+f",x)),
+                          nrow = dim(rscale_scores)[1]),
+          notecol="black",notecex=1, lwid=c(0.01,5),lhei=c(0.01,5),margins =c(.1,.1))
+dev.off()
 
-
-
-
-# 
-# 
-# mapply(function(v,i) v/v[i], far_means_from_0.5,norm_ind)
-# 
-# apply( ,1,function(v,i) v/v[i])
-# 
-# far_norm_means <- far_means_from_0.5/far_means_from_0.5[1]
-# # Write to csv where resultrs from each DF is a column
-# write.table()
-# 
-# dfs_near <- c(ls(pat="dfs_[4-6]a"),ls(pat="dfs_[4-6]b"))
-# 
-# 
-# 
-# items = eapply(.GlobalEnv,function(x) is.list(x) & !is.na(str_match(deparse(substitute(x)), "dfs_\\da")))
-# unlist(items)
-
-# norm_er = 0.5-dfs$df_plotted$mean;
-# write.table(norm_er/norm_er[1], file = far_contest_path, append = TRUE, sep = ",")
 
 
 # Contest 7) which metric is best at discerning experiments below a threshold 
