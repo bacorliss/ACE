9d254486b1abb45377970747207f3352f97f8f0b
Bruce Corliss <bruce.a.corliss@gmail.com>
2020-09-08 09:13:13 -0400

    Added outlines for new figure.

---
+++ figure10_power_contest.R
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+#' Take same simulations fro similarity contests, but sweep the indepdent variable 
+#' and plot error rate with 95% CI for subset of stats across indepdendent variable
+
+
+
+# Calculate weighted power score for null results and positive results
+#     For null results, weight each simulation by the PDF of at that mu/sigma, 
+# scaled across the independent variable
+#     For positive results, weight each simulation by 1-the PDF of at that mu/sigma, 
+# scaled across indepdent variables
---
+++ figure9_consistency_with_similarity.r
@@ -0,0 +1,34 @@
+
+
+
+
+
+# Test how each metric responds to changing each characteristic of similarity
+
+
+
+# Change mu w/ sigma=1, calculate pearson for each stat across mu
+
+# Change sigma at mu=0, calculate pearson for each stat across sigma
+
+# Change sigma at mu=10, calculate pearson for each stat across sigma
+
+
+
+
+# Change rmu w/ sigma=1, calculate pearson for each stat across rmu
+
+# Change rsigma at mu=0, calculate pearson for each stat across rsigma
+
+# Change rsigma at mu=10, calculate pearson for each stat across rsigma
+
+
+# Summarizing table showing MMD is the only stat with a solid color that is statistically significant from
+
+
+
+
+
+
+
+# CHange sampling with fixed std and mu
